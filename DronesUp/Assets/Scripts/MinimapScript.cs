11391bb66629e686fe15d6347d9d4953342f6524
Raja Chellappan <raja.chellappan@outlook.com>
2020-11-02 02:09:06 +0000

    Created Prototype

---
+++ DronesUp/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+
+    public Transform DroneParent;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = DroneParent.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, DroneParent.eulerAngles.y, 0f);
+    }
+
+}
---
+++ DronesUp/Assets/Scripts/PickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holder;
+    
+  
+     
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = holder.position;
+        this.transform.parent = GameObject.Find("Holder").transform;
+
+        
+
+        
+
+    }
+
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+
+}
